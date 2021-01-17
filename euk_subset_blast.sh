11757a8b1dade605395cb3d0887483bc63735784
Amalia M <a.mailli@yahoo.com>
2021-01-18 00:58:50 +0100

    Create euk_subset_blast.sh

---
+++ euk_subset_blast.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+db="/path/to/eukaryote/reference/database"
+blast="/usr/local/bin/blastn"
+##Input from the pipeline
+query=$1
+
+##The input filename is converted for the output filename.
+blastout="$(echo $query| sed 's/.fa/\_euk.bl/')"
+
+##This command line is a typical BLAST command with some very standard parameters that can be altered.
+$blast -db $db -task blastn -query $query -out $blastout -evalue 1E-10 \
+    -outfmt '6 qseqid sseqid stitle staxid qlen slen qstart qend sstart send evalue score length pident nident gapopen gaps qcovs' \
+    -num_threads 25 -max_target_seqs 30 -perc_identity 80
+
+printf "Finished BLAST session against the nr/nt eukaryote domain dataset for file $blastout :\n" >> progress.txt
+date >> progress.txt
+
